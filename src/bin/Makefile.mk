9adc3b72d19eab574f146ca640114c58824ec036
Vincent Torri <vincent.torri@gmail.com>
2014-02-25 15:12:06 +0100

    add icon with a resource file

--- src/bin/Makefile.mk
+++ src/bin/Makefile.mk
@@ -11,7 +11,20 @@ src_bin_etui_LDADD = \
 $(top_builddir)/src/lib/libetui.la \
 @ETUI_BIN_LIBS@
 
-src_bin_etui_elm_SOURCES = src/bin/etui_elm.c
+src_bin_etui_elm_SOURCES = \
+src/bin/etui_elm.c
+
+if HAVE_WIN32
+src_bin_etui_elm_SOURCES += \
+src/bin/etui.rc
+
+windres_verbose = $(windres_verbose_@AM_V@)
+windres_verbose_ = $(windres_verbose_@AM_DEFAULT_V@)
+windres_verbose_0 = $(AM_V_at)echo "  RC      " $@;
+
+.rc.o:
+	$(wc_verbose)windres -o $@ $<
+endif
 
 src_bin_etui_elm_CPPFLAGS = \
 -I$(top_srcdir)/src/lib \
