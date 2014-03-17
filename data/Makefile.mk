bf2020521ab02a4329fd1e0e8106f6a805fedde7
Vincent Torri <vincent dot torri at gmail dot com>
2014-03-17 11:52:53 +0100

    move icons to data/icons/

--- data/Makefile.mk
+++ data/Makefile.mk
@@ -1,67 +1,67 @@
 
 file16dir = $(datadir)/icons/hicolor/16x16/apps
-file16_DATA = data/16x16/etui.png
+file16_DATA = data/icons/16x16/etui.png
 
 file22dir = $(datadir)/icons/hicolor/22x22/apps
-file22_DATA = data/22x22/etui.png
+file22_DATA = data/icons/22x22/etui.png
 
 file24dir = $(datadir)/icons/hicolor/24x24/apps
-file24_DATA = data/24x24/etui.png
+file24_DATA = data/icons/24x24/etui.png
 
 file32dir = $(datadir)/icons/hicolor/32x32/apps
-file32_DATA = data/32x32/etui.png
+file32_DATA = data/icons/32x32/etui.png
 
 file36dir = $(datadir)/icons/hicolor/36x36/apps
-file36_DATA = data/36x36/etui.png
+file36_DATA = data/icons/36x36/etui.png
 
 file48dir = $(datadir)/icons/hicolor/48x48/apps
-file48_DATA = data/48x48/etui.png
+file48_DATA = data/icons/48x48/etui.png
 
 file64dir = $(datadir)/icons/hicolor/64x64/apps
-file64_DATA = data/64x64/etui.png
+file64_DATA = data/icons/64x64/etui.png
 
 file72dir = $(datadir)/icons/hicolor/72x72/apps
-file72_DATA = data/72x72/etui.png
+file72_DATA = data/icons/72x72/etui.png
 
 file96dir = $(datadir)/icons/hicolor/96x96/apps
-file96_DATA = data/96x96/etui.png
+file96_DATA = data/icons/96x96/etui.png
 
 file128dir = $(datadir)/icons/hicolor/128x128/apps
-file128_DATA = data/128x128/etui.png
+file128_DATA = data/icons/128x128/etui.png
 
 file192dir = $(datadir)/icons/hicolor/192x192/apps
-file192_DATA = data/192x192/etui.png
+file192_DATA = data/icons/192x192/etui.png
 
 file256dir = $(datadir)/icons/hicolor/256x256/apps
-file256_DATA = data/256x256/etui.png
+file256_DATA = data/icons/256x256/etui.png
 
 file384dir = $(datadir)/icons/hicolor/384x384/apps
-file384_DATA = data/384x384/etui.png
+file384_DATA = data/icons/384x384/etui.png
 
 file512dir = $(datadir)/icons/hicolor/512x512/apps
-file512_DATA = data/512x512/etui.png
+file512_DATA = data/icons/512x512/etui.png
 
 filesvgdir = $(datadir)/icons/hicolor/scalable/apps
-filesvg_DATA = data/scalable/etui.svg
+filesvg_DATA = data/icons/scalable/etui.svg
 
 filesdir = $(datadir)/applications
 files_DATA = data/desktop/etui.desktop
 
 EXTRA_DIST += \
-data/scalable/etui.svg \
-data/16x16/etui.png \
-data/22x22/etui.png \
-data/24x24/etui.png \
-data/32x32/etui.png \
-data/36x36/etui.png \
-data/48x48/etui.png \
-data/64x64/etui.png \
-data/72x72/etui.png \
-data/96x96/etui.png \
-data/128x128/etui.png \
-data/192x192/etui.png \
-data/256x256/etui.png \
-data/384x384/etui.png \
-data/512x512/etui.png \
-data/ico/etui.ico \
+data/icons/scalable/etui.svg \
+data/icons/16x16/etui.png \
+data/icons/22x22/etui.png \
+data/icons/24x24/etui.png \
+data/icons/32x32/etui.png \
+data/icons/36x36/etui.png \
+data/icons/48x48/etui.png \
+data/icons/64x64/etui.png \
+data/icons/72x72/etui.png \
+data/icons/96x96/etui.png \
+data/icons/128x128/etui.png \
+data/icons/192x192/etui.png \
+data/icons/256x256/etui.png \
+data/icons/384x384/etui.png \
+data/icons/512x512/etui.png \
+data/icons/ico/etui.ico \
 data/desktop/etui.desktop
