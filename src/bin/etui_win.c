78eba6f44b74bc53d60ec6dfd72493bdee965c0f
Vincent Torri <vincent.torri@gmail.com>
2017-04-06 09:09:35 +0200

    display file selector if no name is given in command line

--- src/bin/etui_win.c
+++ src/bin/etui_win.c
@@ -24,6 +24,7 @@
 #include "etui_private.h"
 #include "etui_controls.h"
 #include "etui_main.h"
+#include "etui_open.h"
 #include "etui_theme.h"
 
 
@@ -74,6 +75,17 @@ _etui_mouse_down_cb(void *data,
 
     if (ev->button == 3)
         etui_controls_toggle(etui->window.win, etui->window.base);
+}
+
+static void
+_etui_win_fs_cb(void *data, Evas *e, void *event_info)
+{
+    if (!etui_open_active_get())
+    {
+        Etui *etui = (Etui *)data;
+
+        etui_open_toggle(etui->window.win, etui->window.base);
+    }
 }
 
 static void
@@ -186,6 +198,10 @@ etui_win_new(Etui *etui, const char *role,
                                    _etui_mouse_down_cb, etui);
 
     etui->window.config = config;
+
+    evas_event_callback_add(evas_object_evas_get(etui->window.win),
+                            EVAS_CALLBACK_RENDER_POST,
+                            _etui_win_fs_cb, etui);
 
     return EINA_TRUE;
 }
